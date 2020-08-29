ef00db6bf20016884152c0c817abb89b871d1a4c
Harlan Evans <harlanevans5@gmail.com>
2020-08-29 10:47:15 -0600

    finished wallet and plate

--- plate.rb
+++ plate.rb
@@ -1,9 +1,29 @@
 
 class Plate
+  # attr_accessor :new_item
 
   def initialize
-    @items = []
+    @plate = []
+    # @plate <<  @new_item
+    # puts "Your plate #{@plate}"
   end
+  
+  def add_item_to_plate(new_item)
+    @plate << new_item
+    show_plate
+  end
+
+  def show_plate
+    map_dishes(@plate)
+  end
+  
+  
+  def map_dishes(type_of_dish)
+    puts "Your Plate so far:"
+      type_of_dish.each_with_index do |dish, index|
+        puts "#{index + 1}) #{dish}"
+      end
+    end
 
   # possibly hold two variables
   # 1 for items
